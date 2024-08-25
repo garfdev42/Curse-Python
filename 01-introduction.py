09214b9c2ac5d190384caa209d71c1ae226e3308
garfdev42 <cepedasan007@gmail.com>
2024-08-24 22:34:01 -0500

    Modified introduction.py

--- 01-introduction.py
+++ 01-introduction.py
@@ -8,4 +8,40 @@ print("  /  |")
 print(" /___|")
 
 
+print('Hola Mundo')
+print("Hello world")
+
+
+"""
+Esto es un comentario 
+en varias lineas
+"""
+
+'''
+este tambien es uin comentario 
+de 
+varias 
+lineas 
+'''
+
+#Consultar el tipo de dato 
+print(type("Hola python")) #Tipo str
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
