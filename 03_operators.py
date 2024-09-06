804d236b6b47158872844a1bbba7b7514dca3970
garfdev42 <cepedasan007@gmail.com>
2024-09-06 16:58:24 -0500

    Commit September six

--- 03_operators.py
+++ 03_operators.py
@@ -1,3 +1,5 @@
+##OPERADORES ARITMETICOS
+
 print(23+45)
 print(2-5)
 print(22*4)
@@ -6,10 +8,40 @@ print(3%5)
 print(23//45)
 print(2**5)
 
-print(23+45)
-print(23+45)
-print 
+print("Hola "+"Python "+"¿Que tal?")
+print("Hola "+ str(5))
+print("Hola" * 5)
+print("Hola "*(2**3))
 
-print(23+45)
-print(23+45)
+
+my_float = 2.5 * 2
+print("Holaa " *int(my_float))
+
+print(3< 4)
+print(3 > 4)
+print(3 >= 4)
+print(3 <= 4)
+print(3 == 4)
+print(3!=4)
+
+print("\n")
+
+print("Hola" < "Python")
+print("aaaa" >= "abaa") #Ordenacion alfabetica por ASCII
+print(len("aaaa") >= len("Python"))
+print("Hola" <= "Python")
+print("Hola" == "Hola")
+print("Hola" != "Python")
+
+
+##Operadores Logicos ###
+
+
+
+
+
+  
+
+
+
 
